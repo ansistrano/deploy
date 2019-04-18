452a08e9b4e2bd625cb207e2552047fb83f7d3e6
Adria Galin <adriagalin@gmail.com>
2019-04-18 12:01:13 +0200

    molecule: fix var scope

--- molecule/git/tests/test_git.py
+++ molecule/git/tests/test_git.py
@@ -4,6 +4,3 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-# Make it available for all session (for all tests)
-pytest.deploy_path = "/tmp/git/my-app.com"
