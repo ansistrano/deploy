d1bae835fe3511969e11dca1ce5b711bb8b8104b
Adria Galin <adriagalin@gmail.com>
2019-04-23 18:04:59 +0200

    molecule: templates test

--- molecule/default/tests/conftest.py
+++ molecule/default/tests/conftest.py
@@ -1,4 +1,4 @@
 import pytest
 
 # Make it available for all session (for all tests)
-pytest.deploy_path = "/tmp/download/my-app.com"
+pytest.deploy_path = "/tmp/download/my-app1.com"
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,10 +6,38 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 current_path = pytest.current_path
-archive_name = "1.4.1.zip"
+archive_name = "artifact2.zip"
 
 
 def test_ansistrano_downloaded_files_does_not_exists(host):
     f = host.file(current_path + "/" + archive_name)
 
     assert not f.exists
+
+def test_ansistrano_database_file_exists(host):
+    f = host.file(current_path + "/config/database.yml")
+
+    assert f.exists
+    assert f.user == 'admin'
+    assert f.group == 'admin'
+
+def test_ansistrano_environment_file_exists(host):
+    f = host.file(current_path + "/config/environment.yml")
+
+    assert f.exists
+    assert f.user == 'admin'
+    assert f.group == 'admin'
+
+def test_ansistrano_remote_file_exists(host):
+    f = host.file(current_path + "/config/cycloid.yml")
+
+    assert f.exists
+    assert f.user == 'admin'
+    assert f.group == 'admin'
+
+def test_ansistrano_public_sitemaps_path_exists(host):
+    d = host.file(current_path + "/public/sitemaps")
+
+    assert d.is_symlink
+    assert d.user == 'admin'
+    assert d.group == 'admin'
